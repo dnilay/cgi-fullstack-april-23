bee682ccabd1d9b77c6f811b40b612798f39de3c
Nilay <54245048+dnilay@users.noreply.github.com>
2023-04-27 09:50:32 +0530

    Add files via upload

---
+++ typescript-demo/sample-types.ts
@@ -0,0 +1,9 @@
+let found:boolean=false;
+let grade:number=91.01
+let firstName:string='Marry';
+let lastName:string='Public';
+console.log('first name '+firstName+ ' last name '+lastName+' found? ' +found+' Grade '+grade);
+
+//string templating
+console.log('\n using string templating.......')
+console.log(` first name ${firstName} last name ${lastName} found ${found} grade ${grade}`);
