7e567853e4a95dc6a2e695d1fa60ad87850ef773
Nilay <54245048+dnilay@users.noreply.github.com>
2023-05-16 12:07:43 +0530

    Add files via upload

--- hibernate-demo/src/main/java/org/example/demo/App.java
+++ hibernate-demo/src/main/java/org/example/demo/App.java
@@ -1,5 +1,7 @@
 package org.example.demo;
 
+import java.util.Scanner;
+
 import javax.persistence.Query;
 
 import org.example.demo.model.Student;
@@ -13,20 +15,33 @@ import org.hibernate.cfg.Configuration;
  */
 public class App 
 {
+	static Scanner scanner=new Scanner(System.in);
     public static void main( String[] args )
     {
         SessionFactory factory=new Configuration().configure().addAnnotatedClass(Student.class).buildSessionFactory();
         Session session=factory.openSession();
         session.getTransaction().begin();
-        session.save(new Student(200, "Marry", "Public", "marry@email.com"));
-        System.out.println("student created...");
-        Query query=session.createQuery("FROM Student");
-        java.util.List<Student> students= query.getResultList();
-        for(Student s:students)
-        {
-        	System.out.println(s);
-        }
+		/*
+		 * session.save(new Student(200, "Marry", "Public", "marry@email.com"));
+		 * System.out.println("student created..."); Query
+		 * query=session.createQuery("FROM Student"); java.util.List<Student> students=
+		 * query.getResultList(); for(Student s:students) { System.out.println(s); }
+		 */
         
+        System.out.print("Enter Student ID: ");
+        
+        int id=scanner.nextInt();
+        
+        Student student=session.find(Student.class, id);
+        
+        if(student==null)
+        {
+        	System.out.println("no such id..");
+        }
+        else
+        {
+        	System.out.println("Found: "+student);
+        }
         session.getTransaction().commit();
        
     }
