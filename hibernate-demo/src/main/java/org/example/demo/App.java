564778b6ff8ada8a867a1fcbe9287f4f901b52ac
Nilay <54245048+dnilay@users.noreply.github.com>
2023-05-16 10:57:03 +0530

    Add files via upload

--- hibernate-demo/src/main/java/org/example/demo/App.java
+++ hibernate-demo/src/main/java/org/example/demo/App.java
@@ -1,5 +1,10 @@
 package org.example.demo;
 
+import org.example.demo.model.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SessionFactory factory=new Configuration().configure().addAnnotatedClass(Student.class).buildSessionFactory();
+        Session session=factory.openSession();
+        session.getTransaction().begin();
+        session.save(new Student(100, "John", "Doe", "john@email.com"));
+        session.getTransaction().commit();
+        System.out.println("student created...");
     }
 }
