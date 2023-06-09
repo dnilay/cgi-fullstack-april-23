80992eba188b504d89752ab44611551ad3161217
Nilay <54245048+dnilay@users.noreply.github.com>
2023-06-09 10:45:17 +0530

    Add files via upload

---
+++ order-service/src/main/java/com/example/demo/OrderServiceApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+
+@RestController
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+	@GetMapping
+	public ResponseEntity<?> sayHello(@RequestParam("name") String name)
+	{
+		return ResponseEntity.ok("hello" +name);
+	}
+}
