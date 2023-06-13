311392d770f8b74e13b4aae817737fa8064d3486
Nilay <54245048+dnilay@users.noreply.github.com>
2023-06-13 10:07:16 +0530

    Add files via upload

--- customer-service/src/main/java/com/example/demo/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/example/demo/CustomerServiceApplication.java
@@ -1,42 +1,31 @@
 package com.example.demo;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableEurekaClient
 @SpringBootApplication
-@EnableDiscoveryClient
-
 @RestController
-
-public class OrderServiceApplication {
-	
+public class CustomerServiceApplication {
 	private Environment environment;
-	
-	
 
-	public OrderServiceApplication(Environment environment) {
+	public CustomerServiceApplication(Environment environment) {
 		super();
 		this.environment = environment;
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderServiceApplication.class, args);
+		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
 	@GetMapping
-	public ResponseEntity<?> getStatus()
-	{
-		return ResponseEntity.ok("order-service is up and running on port: "+environment.getProperty("local.server.port"));
+	public ResponseEntity<?> getStatus() {
+		return ResponseEntity
+				.ok("customer-service is up and runing on port: " + environment.getProperty("local.server.port"));
 	}
-
-	
-	
-	
 }
